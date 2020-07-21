8f0d5cc835cd816db9dcc58858e11f59b8d79df6
jomagsm <jomagsm@gmail.com>
2020-07-21 19:41:38 +0600

    подключил db

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    status = models.CharField(max_length=40, null=False, blank=False, default='new')
+    create_at = models.DateTimeField(null=True, blank=True, default='null', verbose_name='Дата выполнения')
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.description)
