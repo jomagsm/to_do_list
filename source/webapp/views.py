24f5037dbcdfbcfd93cdfdf91c8afc3c31a1ccde
jomagsm <jomagsm@gmail.com>
2020-07-22 11:34:28 +0600

    сделал миграцию 0002(Добавил строку name)Добавил name.Подключил index.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from webapp.models import Article
+
+
+def index_view(request):
+    data = Article.objects.all()
+    return render(request, 'index.html', context={
+        'articles': data
+    })
