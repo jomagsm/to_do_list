0da3c299c7182810a1ea2b1b53f08dbe5832b19e
jomagsm <jomagsm@gmail.com>
2020-07-22 22:17:24 +0600

    Поправил add_new статусы и дата (работает коректно), в главную страницу добавил сылки на каждую задачу

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from webapp.models import Article
+from webapp.models import Article, STATUS_CHOICE
 
 
 def index_view(request):
@@ -11,24 +11,15 @@ def index_view(request):
 
 
 def add_new(request):
-    choice = Article.give_choices()
+    choice = STATUS_CHOICE
     if request.method == 'POST':
         name = request.POST.get('name')
         description = request.POST.get('form-description')
         status = request.POST.get('select_status')
         create_at = request.POST.get('finish_data')
-        new_status = None
-        for i in choice:
-            check = status in i
-            if check:
-                ind = i.index(status)
-                ind = ind - 1
-                new_status = choice[ind][0]
-                break
-        status = new_status
-        print(name)
-        # Article.objects.create(name=name, description=description,
-        #                        status=status, create_at=create_at)
+        print(create_at)
+        Article.objects.create(name=name, description=description,
+                               status=status, create_at=create_at)
     return render(request, 'add_new.html', context={
         'choice': choice
     })
